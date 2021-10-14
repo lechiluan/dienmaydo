327470b48765b6f2b0cd674ad2bbe397d71bf280
Lê Chí Luân <chiluan6601@gmail.com>
2021-10-14 14:25:26 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,3 @@
 <?php
-	$Connect = mysqli_connect("ec2-44-199-86-61.compute-1.amazonaws.com","5432","makjgcvtgbfrxg","2a8b99e1f1bece45e7ffcb845684aefae40e4076b876974334bffdedc0f94179","dajuspnve8lvl7") or die("Lỗi".mysqli_error($Connect));
-	mysqli_query($Connect,'SET NAMES "utf8"');
-	//mysqli_close($Connect);
+	$Connect = pg_connect("host=ec2-44-199-86-61.compute-1.amazonaws.com port=5432 dbname=dajuspnve8lvl7 password=2a8b99e1f1bece45e7ffcb845684aefae40e4076b876974334bffdedc0f94179);
+?>
