d5f232ed09bc00bbad2ca711f62254855cc708d0
Lê Chí Luân <chiluan6601@gmail.com>
2021-10-14 17:30:36 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,3 +1,7 @@
 <?php
-	$Connect = pg_connect("host=ec2-44-199-86-61.compute-1.amazonaws.com port=5432 dbname=dajuspnve8lvl7 password=2a8b99e1f1bece45e7ffcb845684aefae40e4076b876974334bffdedc0f94179);
+	$Connect = pg_connect("postgres://makjgcvtgbfrxg:2a8b99e1f1bece45e7ffcb845684aefae40e4076b876974334bffdedc0f94179@ec2-44-199-86-61.compute-1.amazonaws.com:5432/dajuspnve8lvl7");
+	if(!$Connect)
+	{
+		die("Could not connect to database");
+	}
 ?>
