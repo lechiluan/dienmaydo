19df9ae21bf611be262a8e506b8cea95afd9743f
Lê Chí Luân <chiluan6601@gmail.com>
2021-11-17 19:20:34 +0700

    commit to main

--- connect.php
+++ connect.php
@@ -1,7 +1,13 @@
 <?php
+	/*$Connect = mysqli_connect("localhost","root","","qlbh") or die("Lỗi".mysqli_error($Connect));
+	
+	mysqli_query($Connect,'SET NAMES "utf8"');
+	//mysqli_close($Connect);*/
 	$Connect = pg_connect("postgres://makjgcvtgbfrxg:2a8b99e1f1bece45e7ffcb845684aefae40e4076b876974334bffdedc0f94179@ec2-44-199-86-61.compute-1.amazonaws.com:5432/dajuspnve8lvl7");
-	if(!$Connect)
-	{
-		die("Could not connect to database");
-	}
+    //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
+	//$Connect = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=123456");
+	echo 'connected';
+    if (!$Connect) {
+        die("Connection failed");
+    }
 ?>
